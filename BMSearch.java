7f0bd28958fc39e724890dec8a4e868b8283b400
Ten Have <dt194@lab-rg07-25.cms.waikato.ac.nz>
2024-05-06 16:02:27 +1200

    Added initial empty classes needed for the assignment

---
+++ BMSearch.java
@@ -0,0 +1,11 @@
+
+/**Searches for a string pattern in a text file and outputs each line that the string occurs on to the standard output
+ * @author
+ * SID:
+ * Date:
+ */
+public class BMSearch {
+    public static void main(String[] args){
+
+    }
+}
