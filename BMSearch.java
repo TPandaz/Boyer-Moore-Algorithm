ffb675bb931f6f319267fca58c4e614f4e90b00a
Ten Have <dt194@lab-rg07-25.cms.waikato.ac.nz>
2024-05-06 16:24:46 +1200

    Added a simple usage check when running the class

--- BMSearch.java
+++ BMSearch.java
@@ -1,11 +1,18 @@
 
 /**Searches for a string pattern in a text file and outputs each line that the string occurs on to the standard output
- * @author
- * SID:
+ * @author Dolf ten Have
+ * SID: 1617266
  * Date:
  */
 public class BMSearch {
     public static void main(String[] args){
+        String usage = "java BMSearch table.txt [textfile].txt";
 
+        if(args.length != 2){
+            System.err.println("Error: The parameters did not match the propor class usage.\nusage: '" + usage + "'.");
+            return;
+        }
+
+        
     }
 }
