313dfb6625c2ca5b417dbfeaf9b5c3cc3885d778
SamuelTan-Enkai <samueltanenkai@gmail.com>
2024-05-13 20:37:24 +1200

    removed unnecesary logic from MakeBMtable. Updated README file for program

--- MakeBMTable.java
+++ MakeBMTable.java
@@ -1,113 +1,107 @@
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.stream.Collectors;
-import java.lang.StringBuilder;
 
-// 1st case when no suffix and char does not match
-// 2nd case when char doesnt match but there is a matched char in pattern
-// check if either no suffix then value is position of char
-// or suffix matches a pttern in string then move to position
-// 3rd case when char matches
+/**
+ * This program computes the boyer moore skip array by calculating the shift
+ * values for every character in the String. It evaluates the following using
+ * these scenarios:
+ * 1. when character does not match, check if there is a similar character in pattern
+ * 1.a. similar pattern is found, output number of shifts to position of pattern
+ * 1.b. if no similar pattern, then output character length
+ * 2. when character matches, output 0
+ * 
+ * Code used for java streams referenced from:
+ * https://stackoverflow.com/questions/54671799/how-to-sort-and-eliminate-the-duplicates-from-arraylist/54671928
+ * 
+ * @author: Samuel Tan
+ * Student ID: 1617175
+ */
 public class MakeBMTable {
-  public static void main(String[] args) {
-    if (args.length != 2) {
-      System.err.println("Usage: java MakeBMTable [pattern] outputFile");
-      System.exit(1);
-    }
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Usage: java MakeBMTable <pattern> <outputFile>");
+            System.exit(1);
+        }
 
-    String pattern = args[0];
-    String outputFile = args[1];
-    try {
-      File file = new File(outputFile);
-      PrintWriter writer = new PrintWriter(file, "UTF-8");
-      // write the first row
-      writer.print('*');
-      for (char c : pattern.toCharArray()) {
-        writer.print(',');
-        writer.print(c);
-      }
+        // place arguments into variables
+        String pattern = args[0];
+        String outputFile = args[1];
+        try {
+            File file = new File(outputFile);
+            PrintWriter writer = new PrintWriter(file, "UTF-8");
 
-      // get 1st column of characters in distinct and sorted order using java streams
-      ArrayList<Character> uniqueChars = pattern.chars().distinct().sorted().mapToObj(c -> (char) c)
-          .collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
-      uniqueChars.add('*');
-
-      Deque<Integer> row = new ArrayDeque<>();
-      StringBuilder suffixValues = new StringBuilder();
-      // for every row in column
-      for (int i = 0; i < uniqueChars.size(); i++) {
-        for (int j = pattern.length() - 1; j >= 0; j--) {
-          // check if char in row matches column
-          if (uniqueChars.get(i) != pattern.charAt(j)) {
-            // if there is no suffix present (1st char in row being compared)
-            if (j == pattern.length() - 1) {
-              // then check if there is another instance of char in pattern that matches the
-              // char in row
-              if (pattern.contains(String.valueOf(uniqueChars.get(i)))) {
-                // insert into row the skip value
-                int skipValue = j - (pattern.lastIndexOf(uniqueChars.get(i)));
-                row.push(skipValue);
-              } else {
-                // this means there is no suffix and no char in row matches, so input length of
-                // pattern
-                row.push(pattern.length());
-              }
-            } else {
-              // there is a suffix, so check if there is a pattern in suffix that matches
-              // pattern in row
-              // if not, then output length of pattern
-
-              StringBuilder patternToBeMatched = new StringBuilder(suffixValues);
-              patternToBeMatched.insert(0, uniqueChars.get(i));
-              
-              for (int k = pattern.length(); k > 0; k--) {
-                // check if suffix matches the pattern at index
-                if (pattern.substring(k - patternToBeMatched.length(), k).equals(patternToBeMatched.toString())) {
-                  //if so, shift value is difference between pattern length and k(the count of right shifts made)
-                  int shiftValue = pattern.length() - k;
-                  row.push(shiftValue);
-                  break;
-                }
-                // remove first char from patternToBeMatched if shift exceeds the 1st index of
-                // pattern
-                if ((k - patternToBeMatched.length()) == 0) {
-                  patternToBeMatched.deleteCharAt(0);
-                }
-              }
-
-              // check if matchedPrefix is empty, if so, no suffix matched
-              if (patternToBeMatched.length() == 0) {
-                row.push(pattern.length());
-                System.out.println("inserted: " + pattern.length());
-              }
-
+            // write the first row to outputFile
+            writer.print('*');
+            for (char c : pattern.toCharArray()) {
+                writer.print(',');
+                writer.print(c);
             }
 
-          } else {
-            // pattern matches so input 0
-            row.push(0);
-          }
+            // get 1st column of characters in distinct and sorted order using java stream
+            ArrayList<Character> uniqueChars = (ArrayList<Character>) pattern.chars().distinct().sorted()
+                    .mapToObj(c -> (char) c)
+                    .collect(Collectors.toList());
+            uniqueChars.add('*');
 
-          suffixValues.insert(0, pattern.charAt(j));
-          System.out.println(uniqueChars.get(i) + ": " + suffixValues);
+            // deque for row used as we are inserting to the front of the queue
+            Deque<Integer> row = new ArrayDeque<>();
+            StringBuilder suffixValues = new StringBuilder();
 
+            // for every row in column
+            for (int i = 0; i < uniqueChars.size(); i++) {
+                for (int j = pattern.length() - 1; j >= 0; j--) {
+                    // check if character in row does not matches
+                    if (uniqueChars.get(i) != pattern.charAt(j)) {
+                        // check if there is a pattern/character in row that matches the character(and the suffix if it has one)
+                        StringBuilder patternToBeMatched = new StringBuilder(suffixValues);
+                        patternToBeMatched.insert(0, uniqueChars.get(i)); // add the char
+
+                        // for every iteration, shift suffix to the left, comparing it with the pattern
+                        for (int k = pattern.length(); k > 0; k--) {
+                            // check if suffix matches the pattern at index k
+                            if (pattern.substring(k - patternToBeMatched.length(), k)
+                                    .equals(patternToBeMatched.toString())) {
+                                // if so, the shiftValue is the difference between pattern length and k(count of left shifts made)
+                                row.push(pattern.length() - k);
+                                break;
+                            }
+                            // check if patternToBeMatched is at index 0 of pattern(number of leftshifts)
+                            if ((k - patternToBeMatched.length()) == 0) {
+                                // remove the 1st character for next iteration
+                                patternToBeMatched.deleteCharAt(0);
+                            }
+                        }
+
+                        // check if there is no pattern matching, input pattern length if so
+                        if (patternToBeMatched.length() == 0) {
+                            row.push(pattern.length());
+                        }
+
+                    } else {
+                        // character matches so input 0
+                        row.push(0);
+                    }
+
+                    // insert the character into suffix
+                    suffixValues.insert(0, pattern.charAt(j));
+                }
+                writer.println();
+                writer.print(uniqueChars.get(i) + ",");
+                // using java streams, print formatted row
+                writer.print(row.stream().map(Object::toString).collect(Collectors.joining(",")));
+                row.clear();
+                // clear suffix values
+                suffixValues.setLength(0);
+            }
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        // row is Int values
-        writer.println();
-        writer.print(uniqueChars.get(i) + ",");
-        writer.print(row.stream().map(Object::toString).collect(Collectors.joining(",")));
-        row.clear();
-        suffixValues.setLength(0);
-
-      }
-      writer.flush();
-      writer.close();
-    } catch (IOException e) {
-      e.printStackTrace();
     }
-
-  }
-
 }
--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 #### MakeBMTable.java
 This program takes in a string and a filename from the console. Using the string, this program calculates the shift values for every character in the string following the Boyer Moore Algorithm. Writing the values to the filename specified in the form of a skip table.
 
+Note: The character '*' represents any character that is not present in the pattern, and is being used for comparisons in the table. In the case where the pattern to be searched contains a '*', another placeholder character needs to be hardcoded into the program to ensure skip values for the table are accurate.
+
 Example Usage: java MakeBMTable "kokako" table.txt
 
 #### BMSearch.java
